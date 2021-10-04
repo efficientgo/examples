d66985a3d893e7486769e8295248df7be277f112
Bartlomiej Plotka <bwplotka@gmail.com>
2021-10-04 09:05:10 +0200

    Update.
    Signed-off-by: Bartlomiej Plotka <bwplotka@gmail.com>

--- pkg/dups/dup_test.go
+++ pkg/dups/dup_test.go
@@ -58,28 +58,32 @@ cpu: Intel(R) Core(TM) i7-9850H CPU @ 2.60GHz
 BenchmarkDeduplicate
 BenchmarkDeduplicate/all_duplicates
 BenchmarkDeduplicate/all_duplicates/fast
-BenchmarkDeduplicate/all_duplicates/fast-12    	    1340	   3859073 ns/op	       8 B/op	       1 allocs/op
+BenchmarkDeduplicate/all_duplicates/fast-12    	    1531	   (3.98s) 3980299 ns/op	       8 B/op	       1 allocs/op
 BenchmarkDeduplicate/all_duplicates/fastLessAllocs
-BenchmarkDeduplicate/all_duplicates/fastLessAllocs-12         	     628	  11638731 ns/op	22282269 B/op	       2 allocs/op
+BenchmarkDeduplicate/all_duplicates/fastLessAllocs-12         	     585	   9961814 ns/op	22282268 B/op	       2 allocs/op
 BenchmarkDeduplicate/all_duplicates/fastLessAllocs2
-BenchmarkDeduplicate/all_duplicates/fastLessAllocs2-12        	     384	  15817382 ns/op	22282290 B/op	       3 allocs/op
+BenchmarkDeduplicate/all_duplicates/fastLessAllocs2-12        	     460	  12815054 ns/op	22282289 B/op	       3 allocs/op
 BenchmarkDeduplicate/all_duplicates/fastDynamic
-BenchmarkDeduplicate/all_duplicates/fastDynamic-12            	    2906	   2041365 ns/op	       8 B/op	       1 allocs/op
+BenchmarkDeduplicate/all_duplicates/fastDynamic-12            	    2666	   2032222 ns/op	       8 B/op	       1 allocs/op
 BenchmarkDeduplicate/all_duplicates/fastDynamicLessAllocs
-BenchmarkDeduplicate/all_duplicates/fastDynamicLessAllocs-12  	    2818	   2029504 ns/op	       0 B/op	       0 allocs/op
+BenchmarkDeduplicate/all_duplicates/fastDynamicLessAllocs-12  	    2540	   2077542 ns/op	       0 B/op	       0 allocs/op
+BenchmarkDeduplicate/all_duplicates/fastDynamicLessAllocs2
+BenchmarkDeduplicate/all_duplicates/fastDynamicLessAllocs2-12 	    3046	   1997189 ns/op	       0 B/op	       0 allocs/op
 BenchmarkDeduplicate/all_duplicates/naive
 BenchmarkDeduplicate/all_duplicates/naive-12                  	    3093	 (2.08ms)  2084348 ns/op	       8 B/op	       1 allocs/op
 BenchmarkDeduplicate/no_duplicates
 BenchmarkDeduplicate/no_duplicates/fast
-BenchmarkDeduplicate/no_duplicates/fast-12                    	      42	 128540361 ns/op	95011732 B/op	   38381 allocs/op
+BenchmarkDeduplicate/no_duplicates/fast-12                    	      39	 129975298 ns/op	95011827 B/op	   38381 allocs/op
 BenchmarkDeduplicate/no_duplicates/fastLessAllocs
-BenchmarkDeduplicate/no_duplicates/fastLessAllocs-12          	      42	 164707819 ns/op	22439210 B/op	      20 allocs/op
+BenchmarkDeduplicate/no_duplicates/fastLessAllocs-12          	      57	  92966844 ns/op	22439211 B/op	      20 allocs/op
 BenchmarkDeduplicate/no_duplicates/fastLessAllocs2
-BenchmarkDeduplicate/no_duplicates/fastLessAllocs2-12         	      75	 125078279 ns/op	22439212 B/op	      20 allocs/op
+BenchmarkDeduplicate/no_duplicates/fastLessAllocs2-12         	      66	  82490873 ns/op	22439215 B/op	      20 allocs/op
 BenchmarkDeduplicate/no_duplicates/fastDynamic
-BenchmarkDeduplicate/no_duplicates/fastDynamic-12             	      44	 133061756 ns/op	95012514 B/op	   38366 allocs/op
+BenchmarkDeduplicate/no_duplicates/fastDynamic-12             	      44	 132438695 ns/op	95013322 B/op	   38382 allocs/op
 BenchmarkDeduplicate/no_duplicates/fastDynamicLessAllocs
-BenchmarkDeduplicate/no_duplicates/fastDynamicLessAllocs-12   	      42	 119816109 ns/op	22439256 B/op	      21 allocs/op
+BenchmarkDeduplicate/no_duplicates/fastDynamicLessAllocs-12   	      55	  97527610 ns/op	22439256 B/op	      21 allocs/op
+BenchmarkDeduplicate/no_duplicates/fastDynamicLessAllocs2
+BenchmarkDeduplicate/no_duplicates/fastDynamicLessAllocs2-12  	      62	  85723485 ns/op	22439261 B/op	      21 allocs/op
 BenchmarkDeduplicate/no_duplicates/naive
 BenchmarkDeduplicate/no_duplicates/naive-12                    	       1	(6.0946657054m) 365679942324 ns/op	(45MB) 45188352 B/op (45MB)	      40 allocs/op (0.365679942 N^2) ns
 PASS
@@ -113,8 +117,8 @@ func BenchmarkDeduplicate(b *testing.B) {
 				{name: "fastLessAllocs2", dedupFunc: DeduplicateLessAllocs2},
 				{name: "fastDynamic", dedupFunc: DeduplicateDynamic},
 				{name: "fastDynamicLessAllocs", dedupFunc: DeduplicateDynamicLessAllocs},
-				{name: "fastDynamicLessAllocs2", dedupFunc: DeduplicateDynamicLessAllocs},
-				//{name: "naive", dedupFunc: DeduplicateNaively},
+				{name: "fastDynamicLessAllocs2", dedupFunc: DeduplicateDynamicLessAllocs2},
+				//{name: "naive", dedupFunc: DeduplicateNaively}, // Beware, takes ages!
 			} {
 				b.Run(funcCase.name, func(b *testing.B) {
 					b.ReportAllocs()
